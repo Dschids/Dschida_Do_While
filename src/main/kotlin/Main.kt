83b376a46f6e8205280f3212310b5f0adc540220
Dschids <123191756+Dschids@users.noreply.github.com>
2023-08-28 19:40:45 -0500

    added when statement inside loop to determine output comments

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,11 @@
 fun main(args: Array<String>) {
 /* program presents user with a list of 5 choices if the user picks a valid choice it is
-displayed and the program ends. If a user selects on invalid option they recieve an error and the
+displayed and the program ends. If a user selects on invalid option they receive an error and the
 choices are displayed again
  */
 var userchoice = 0
-
+var output = "x"
+//  do while that runs as long as the user selects a number outside 1..5
 do {
     println("How confident are you that your code will work?")
     println("1) It's definitely going to work!)")
@@ -13,8 +14,18 @@ do {
     println("4) No way in hell this works")
     println("5) No Comment")
     userchoice = readln()!!.toInt()
-    if (userchoice > 5){
-        println("Invalid choice!")
+    // when statement to determine what to print
+    when (userchoice){
+        1 -> output = "1) It's definitely going to work!"
+        2 -> output = "2) It will probably work"
+        3 -> output = "3) It might not work"
+        4 -> output = "4) No way in hell this works"
+        5 -> output = "5) No Comment"
+        !in 1..5 -> output = "$userchoice Is not a valid selection, try again"
+        else -> output = "Unknown"
     }
+    println("You have selected:")
+    println(output)
+
 } while (userchoice > 5)
 }
